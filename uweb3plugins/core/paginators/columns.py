7b3b2837dd41fe1339df9030b0e8318ca4920ae4
Stef <stefvanhouten@gmail.com>
2022-09-26 14:24:25 +0200

    Move docstrings to __init__

--- uweb3plugins/core/paginators/columns.py
+++ uweb3plugins/core/paginators/columns.py
@@ -9,42 +9,57 @@ class MyFormatter(string.Formatter):
 
 
 class Col:
-    """Base column class that can be used to render simple
-    columns in a table.
-
-    Example usage:
-        class Items:
-            ID: int
-            name: str
-
-        class ItemsTable(BasicTable):
-            id = Col("ID", "ID")
-            name = Col("Name", "name")
-
-        table = ItemsTable(items=[Items(ID=1, name="foo"), Items(ID=2, name="bar")])
-
-        The rendered table will look like this:
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>foo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>bar</td>
-                    </tr>
-                </tbody>
-            </table>
-    """
-
     def __init__(self, name, attr, sortable=False, enabled=True):
+        """Base column class that can be used to render simple
+        columns in a table.
+
+        Args:
+            name (str): The title of the column, this will be displayed
+                in the <th> element.
+            attr (str): The attribute name of the value that should be
+                retrieved from the item. When the item has nested children
+                (because of lazyloading) or the item is a dict with nested
+                keys the following syntax can be used to extract the value
+                from a nested object: "foo.bar.baz". This will return the
+                value of item["foo"]["bar"]["baz"].
+            sortable (bool, optional): Determines if the column is sortable.
+                Defaults to False.
+            enabled (bool, optional): Determine wheter or not
+                this column header and value should be displayed.
+                Defaults to True.
+
+        Example usage:
+            class Items:
+                ID: int
+                name: str
+
+            class ItemsTable(BasicTable):
+                id = Col("ID", attr="ID")
+                name = Col("Name", attr="name")
+
+            table = ItemsTable(items=[Items(ID=1, name="foo"), Items(ID=2, name="bar")])
+
+            The rendered table will look like this:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>foo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>bar</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+        """
         self.name = name
         self.attr = attr
         self.sortable = sortable
@@ -55,77 +70,76 @@ class Col:
 
 
 class LinkCol(Col):
-    """Adds a row to the table of the following format:
-        <td>
-            <a href="href">attr</a>
-        </td>
-
-    The value that is displayed is the passed attr value.
-    The column will be populated with data on render, the
-    value in the href will be retrieved from the current item
-    that is being iterated over.
-
-    Retrival of nested values or children from uweb3 model
-    objects is supported. For example if we have a record
-    class that looks like this:
-
-        class Client(uweb3.model.Record):
-            ID: int
-            name: str
-
-        class Invoice(uweb3.model.Record):
-            ID: int
-            title: str
-            client: Client.ID
-
-        class Table(BasicTable):
-            col1 = LinkCol("Client", attr="client.name", href="/client/{client.ID}")
-            col2 = LinkCol("Invoice", attr="title", href="/invoices/{ID}")
-
-        Table(items=[Invoice(1, "Invoice 1", Client(1, "Client 1"))])
-
-        The rendered table will look like this:
-            <table>
-                <thead>
-                    <tr>
-                        <th>Client</th>
-                        <th>Invoice</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <a href="/client/1">Client 1</a>
-                        </td>
-                        <td>
-                            <a href="/invoices/1">Invoice 1</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-    It is also possible to pass an href without any fields:
-        class Table(BasicTable):
-            LinkCol("Invoice", attr="title", href="/invoices")
-
-        which will result in the following table:
-            <table>
-                <thead>
-                    <tr>
-                        <th>Invoice</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <a href="/invoices">Invoice 1</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-    """
-
     def __init__(self, name, attr, href, *args, **kwargs):
+        """Adds a row to the table of the following format:
+            <td>
+                <a href="href">attr</a>
+            </td>
+
+        The value that is displayed is the passed attr value.
+        The column will be populated with data on render, the
+        value in the href will be retrieved from the current item
+        that is being iterated over.
+
+        Retrival of nested values or children from uweb3 model
+        objects is supported. For example if we have a record
+        class that looks like this:
+
+            class Client(uweb3.model.Record):
+                ID: int
+                name: str
+
+            class Invoice(uweb3.model.Record):
+                ID: int
+                title: str
+                client: Client.ID
+
+            class Table(BasicTable):
+                col1 = LinkCol("Client", attr="client.name", href="/client/{client.ID}")
+                col2 = LinkCol("Invoice", attr="title", href="/invoices/{ID}")
+
+            Table(items=[Invoice(1, "Invoice 1", Client(1, "Client 1"))])
+
+            The rendered table will look like this:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Client</th>
+                            <th>Invoice</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="/client/1">Client 1</a>
+                            </td>
+                            <td>
+                                <a href="/invoices/1">Invoice 1</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+        It is also possible to pass an href without any fields:
+            class Table(BasicTable):
+                LinkCol("Invoice", attr="title", href="/invoices")
+
+            which will result in the following table:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Invoice</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="/invoices">Invoice 1</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+        """
         super().__init__(name, attr, *args, **kwargs)
         self.href = href
 
